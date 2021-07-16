ce9e00876d64c0d1e3620e5cf55c9fa398a6fe82
omnisirus <smirnovceo@yandex.ru>
2021-07-17 01:32:07 +0500

    added is_anabled() method in test_items.py

--- lesson36_step9/test_items.py
+++ lesson36_step9/test_items.py
@@ -1,12 +1,12 @@
 from typing import Union
 from selenium import webdriver
+import time
 
 
 def test_add_to_cart_button(browser: Union[webdriver.Chrome, webdriver.Firefox]):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     browser.implicitly_wait(5)
-    button = browser.find_element_by_css_selector('.btn-add-to-basket')
-    button.click()
-    alert = browser.find_element_by_tag_name('strong').text
-    assert alert, 'alert is not "Deferred benefit offer"'
+    button = browser.find_element_by_css_selector('.btn-add-to-basket').is_enabled()
+    time.sleep(10)
+    assert button, 'alert is not "Deferred benefit offer"'
