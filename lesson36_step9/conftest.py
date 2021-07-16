75fd9c614e5515ace26888b10e021b6a41992b45
omnisirus <smirnovceo@yandex.ru>
2021-07-17 02:04:23 +0500

    refactoring conftest.py browser func

--- lesson36_step9/conftest.py
+++ lesson36_step9/conftest.py
@@ -27,5 +27,6 @@ def browser(request):
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError('--browser_name should be chrome or firefox')
+    
     yield browser
     browser.quit()
